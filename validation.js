ebcae3b7cc525cbad7a772ed39c25a37ed8b0954
Bryan Gamotea <bryan.gamotea26@gmail.com>
2013-12-09 18:17:17 +0800

    Validate.

--- validation.js
+++ validation.js
@@ -1,31 +1,39 @@
 $(document).ready(function() {
 
-function FormValidation () 
-{
-	var valid = parseInt($("#validate").val());
-	 if (valid == null || valid == " " || valid !== (/^[0-9]+$/)){
-		$("#errLog").text("You may have entered a non-numeric input or the text box maybe blank! ");
-	}
+	function FormValidation () 
+	{
+		var valid = $("#validate").val();
+		console.log(valid !== (/^[0-9]+$/));
+		if (valid == null || valid == " " || valid !== (/^[0-9]+$/))
+		{
 
-	 
-	 for (var i = valid; i >= 0; i--) {
+			$("#errLog").text("You may have entered a non-numeric input or the text box maybe blank! ");
 
-	 if (valid == 0) {
-		$("#errLog").text("Nothing to do, you entered a zero.");	
-	} 
-
-	else if (valid == 1) {
-		$("#errLog").text("Thank you for entering the number 1");
-		
-	} else{
-	 	console.log(i);
-		$("#count").text("-countdown: " + i);
 		}
-	 };
 
-	 
-	 
-}
+		if (valid == "0")
+		{
+
+			$("#errLog").text("Nothing to do, you entered a zero.");	
+			
+		} 
+
+		else if (valid == "1")
+		{
+			$("#errLog").text("Thank you for entering the number 1");
+
+		}
+		else if (valid > 1)
+		{
+			x = " ";
+			for (var i = valid; i >= 0; i--)
+			{
+				console.log(i);
+				x = x + "countdown: " + i + "<br>";
+			}
+				$("#errLog").html(x);
+		}
+	}
 
 $("#submitbtn").click(FormValidation);
 
