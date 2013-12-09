d466addbf49bc683b2d8721d6617499ef6d12c4e
Bryan Gamotea <bryan.gamotea26@gmail.com>
2013-12-09 13:30:34 +0800

    Fixed bugs on animation.

--- tfscript.js
+++ tfscript.js
@@ -2,24 +2,43 @@ $(document).ready(function () {
 
 
 //functions 
-	function onleftclick() {
-		$(".imgslider img:last-child").prependTo(".imgslider");
-		$(".imgslider").css({"margin-left": "-26%"});
-		$(".imgslider").animate({"margin-left": "0px"},150,function(){
+	function onleftclick() {	
+		$(".sliderWrapper .item:last-child").prependTo(".imgslider");
+		$(".imgslider").css({"margin-left": "-330px"});
+		$(".imgslider").animate({"margin-left": "0px"},200,function(){
 
 		});
 	}
 
 	function onrightclick() {
-		$(".imgslider").animate({"margin-left": "-26%"},150,function(){
-			$(".imgslider img:first-child").appendTo(".imgslider");
+		$(".imgslider").animate({"margin-left": "-330px"},200,function(){
+			$(".imgslider .item:first-child").appendTo(".imgslider");
 			$(".imgslider").css({"margin-left": "0px"});
 	
 	});
 }
 
-	// events
+
+// events
 	$("#leftbtn").click(onleftclick);
 	$("#rightbtn").click(onrightclick);
 
+	$(".item-content").hover(function(){
+		$(this).animate({"margin-top": "230px"},200, function(){
+			
+		});
+
+		$(this).find(".open").animate({"padding-right": "20px"},200, function(){
+
+		});
+	
+	}, function(){
+		$(this).animate({"margin-top": "250px"},200,function(){
+
+		});
+
+		$(this).find(".open").animate({"padding-right": "0px"},200, function(){
+			
+		});
+	});
 });
