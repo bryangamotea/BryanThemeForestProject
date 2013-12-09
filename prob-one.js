9d078b67906ae9658279d3106814255eee646ef3
Bryan Gamotea <bryan.gamotea26@gmail.com>
2013-12-10 04:28:42 +0800

    Problem one complete.

--- prob-one.js
+++ prob-one.js
@@ -1,3 +1,18 @@
 $(document).ready(function(){
-	
+	$(".errlist").empty();
+	function max(maxOne, maxTwo)
+	{
+		if (maxOne > maxTwo)
+		{
+			return maxOne;
+		} else if (maxOne < maxTwo) {
+			return maxTwo;
+		} else {
+			return $(".errlist").html("<li>Numbers are equal.</li>");
+		}
+	}
+
+	var maxOne = prompt("number 1: ");
+	var maxTwo = prompt("number 2: ");
+	$("<li>"+ max(maxOne,maxTwo) + "</li>").appendTo(".errlist")
 });
