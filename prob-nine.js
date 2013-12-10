d49e794759f6dd3154f854847e57dfe98f65ddf1
Bryan Gamotea <bryan.gamotea26@gmail.com>
2013-12-10 18:38:34 +0800

    problem nine solved.

---
+++ prob-nine.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+	var i = 7;
+	var myArray = new Array("efren","gamotea","bantug","lim", "asdfghjjk");
+	function filterLongWords(filter){
+		for(var b= 0; b < myArray.length;b++) {
+			if (myArray[b].length > i){
+				return myArray[b];
+			}
+		}
+	
+	}
+	$("#submitbtn").click(function(){
+		var result = filterLongWords(myArray);
+		$(".errlist").html("<li>"+ result + "</li>");
+	});
+
+});
