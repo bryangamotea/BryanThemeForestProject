2616aa81117e5706fc5a5aecafc786c4afa04578
Bryan Gamotea <bryan.gamotea26@gmail.com>
2013-12-11 09:31:15 +0800

    Problem seven solved.

--- prob-seven.js
+++ prob-seven.js
@@ -1,15 +1,22 @@
 $(document).ready(function(){
 	
-	function sort()
-	{
-		var myArray = new Array("bryan","ryan","yan","an");
-		myArray.sort();
-		x = " ";
-		for (var i = 0; i < myArray.length ; i++) {
+	var myArray = new Array("bryan","ryan","yan","an");
+	myArray.sort();
+	x = " ";
+	function sortElement(bryArr){
+		
+		for (var i = 0; i < bryArr.length ; i++) {
 			console.log("test");
-			x = x + "<li>" + myArray[i] + "</li>";
-			$(".errlist").html(x);
+			// x = x  + bryArr[i];
+			// $(".errlist").html(x);
+			bryArr[i];
 		}
+		return bryArr;
 	}
-	$("#submitbtn").click(sort);
+
+	$("#submitbtn").click(function(){
+		var result = sortElement(myArray);
+		$(".errlist").html("<li>" + result + "</li>");
+	});
+
 });
